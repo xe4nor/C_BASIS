5203e362ae8cc86c5de209ae93d2ed5b848c6bc5
xe4nor <bachmanndominguezjan@gmail.com>
2025-06-27 09:38:41 +0200

    Update CreateProcessW_1.c

--- WIN32_API/CreateProcessW/No1/CreateProcessW_1.c
+++ WIN32_API/CreateProcessW/No1/CreateProcessW_1.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
 
+int main()
+{
+    STARTUPINFOW si;
+
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    //Pfad zum Programm der gestartet werden soll
+    LPCWSTR appPath = L"C:\\Pfad\\Zum\\Programm.exe"; // ersetzen durch programm
+
+    //Prozess erstellen
+
+    if (CreateProcessW(
+        appPath,            //Anwendungspfad
+        NULL,               //Befehlszeile kann auch Parameter enthalten
+        NULL,               //Prozess Sicherheitsatributte 
+        NULL,               //Thread Sicherheitsattribute
+        FALSE,              //HANDLE Vererbung
+        0,                  //Erstellungflags
+        NULL,               //Umgebungsvariablen
+        NULL,               //Arbeitsverzeichniss
+        &si,                //Startupinfo
+        &pi                 //prozessinfo
+    )){
+        Wprintf(L"Prozess erfolgreich erstellt PID: %lu\n", pi.dwProcessId);
+
+        //optional
+        WaitForSingleObject(pi.hProcess, INFINITE);
+
+        //handles Schließen
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+
+    } else {
+        wprintf("fehler beim erstellen des prozesses Fehler: %lu", GetLastError());
+    }
+
+    return 0;
+}
